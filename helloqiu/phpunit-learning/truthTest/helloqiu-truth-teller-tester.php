7030403dc06331c8ae50403d208f17c5fd3ad71c
helloqiu <695441493@qq.com>
2015-09-06 16:45:35 +0800

    begin to learn the depends

--- helloqiu/phpunit-learning/truthTest/helloqiu-truth-teller-tester.php
+++ helloqiu/phpunit-learning/truthTest/helloqiu-truth-teller-tester.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'helloqiu-truth-teller.php';
-class Helloqiu_Truth_Teller_Texter extends PHPUnit_Framework_TestCase{
+class Helloqiu_Truth_Teller_Tester extends PHPUnit_Framework_TestCase{
 	function testTruthTeller() {
 		$tt = new Helloqiu_Truth_Teller();
 		$this -> assertTrue( $tt -> tell_truth() );
